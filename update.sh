bb275a1edc7d17f43164f8b5e21d963b570501a6
ironmaidenopera <oleksander.palii@gmail.com>
2025-08-15 19:45:07 +0300

    commented

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e
-git add .
-git commit -m "$1"
-git push origin main
+# this is a script that runs commits pushes them online
+set -e			# exit on error
+git add .		# stage all files but .gitignore
+git commit -m "$1"	# create a commit with a meassage 
+			# passed on script run; else aborts
+git push origin main	# push commits to the remote origin 
+			# branch main
