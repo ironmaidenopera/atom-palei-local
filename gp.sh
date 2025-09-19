d29f1314af87dc32f7d0116d63b80e6ce5e1bfe2
ironmaidenopera <oleksander.palii@gmail.com>
2025-09-20 00:54:51 +0300

    md test

--- gp.sh
+++ gp.sh
@@ -3,11 +3,6 @@ set -e
 
 : "${1?Usage: $0 \"commit message\"}"
 
-# Only proceed if there are changes (staged or unstaged)
-if git diff --quiet && git diff --cached --quiet; then
-  echo "No changes to commit."
-  exit 0
-fi
 # Detect *any* changes
 if ! git status --porcelain | grep .; then
     echo "No changes to commit."
